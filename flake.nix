2b8182206762cbbef279d88520c5917735592c6e
FPC5719 <fpc5719@163.com>
2025-05-14 02:30:26 +0000

    Initial Config

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "https://mirrors.ustc.edu.cn/nix-channels/nixos-24.11/nixexprs.tar.xz";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs    = true;
+          home-manager.useUserPackages  = true;
+          home-manager.users.nixos      = import ./home.nix;
+          home-manager.extraSpecialArgs = inputs;
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, ... }:
+
+{
+  home.username      = "nixos";
+  home.homeDirectory = "/home/nixos";
+
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  home.packages = with pkgs; [
+    neofetch nnn
+    zip unzip xz  # archives
+
+    file which tree
+    gnused gnutar gawk zstd gnupg
+
+    # nix related
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    htop
+  ];
+
+  programs.git = {
+    enable     = true;
+    userName   = "FPC5719";
+    userEmail  = "fpc5719@163.com";
+  };
+
+  programs.bash = {
+    enable           = true;
+    enableCompletion = true;
+    bashrcExtra      = ''
+      # export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
+    '';
+    shellAliases     = {
+      em = "emacs -nw";
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
