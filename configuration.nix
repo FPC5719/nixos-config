98288b43264c71f629870be35d9de3865a4b0a3d
FPC5719 <fpc5719@163.com>
2025-09-16 08:50:23 +0000

    Enable C-Cache

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,7 @@
     builders-use-substitutes = true;
     experimental-features = [ "nix-command" "flakes" ];
     auto-optimise-store = true;
+    extra-sandbox-paths = [ config.programs.ccache.cacheDir ];
   };
 
   # Optimize Storage
@@ -38,6 +39,37 @@
     dates = "weekly";
     options = "--delete-older-than 1w";
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      ccacheWrapper = super.ccacheWrapper.override {
+        extraConfig = ''
+        export CCACHE_COMPRESS=1
+        export CCACHE_DIR="${config.programs.ccache.cacheDir}"
+        export CCACHE_UMASK=007
+        export CCACHE_SLOPPINESS=random_seed
+        if [ ! -d "$CCACHE_DIR" ]; then
+          echo "====="
+          echo "Directory '$CCACHE_DIR' does not exist"
+          echo "Please create it with:"
+          echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+          echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+          echo "====="
+          exit 1
+        fi
+        if [ ! -w "$CCACHE_DIR" ]; then
+          echo "====="
+          echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+          echo "Please verify its access permissions"
+          echo "====="
+          exit 1
+        fi
+      '';
+      };
+    })
+  ];
+
+  programs.ccache.enable = true;
 
   environment.systemPackages =
     with pkgs; [
